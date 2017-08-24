7e1c7f4dfc7500fbafbc16d33c355514861b2b00
nizam432 <nizam.fci@gmail.com>
2017-08-24 19:10:26 +0600

    update

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -54,7 +54,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Logo -->
     <a href="<?php echo base_url();?>backend_dashboard" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>Z</b>SK</span>
+      <span class="logo-mini"><b>Z</b>SU</span>
       <!-- logo for regular state and mobile devices -->
       <span class="logo-lg"><b>Sikder</b> University</span>
     </a>
