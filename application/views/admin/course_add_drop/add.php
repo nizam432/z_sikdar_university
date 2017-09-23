0fc40df2070efc134be472f4b13927cb04d64ce8
MD NIZAM UDDIN <nizam.fci@gmail.com>
2017-09-23 13:04:54 +0600

    update

--- application/views/admin/course_add_drop/add.php
+++ application/views/admin/course_add_drop/add.php
@@ -76,66 +76,7 @@
 <div id="student_registerd_course"></div>
 <div id="course_add_drop"></div>
 
-<<<<<<< HEAD
-			<tbody id="data_load_list">
-			<?php foreach($get_assing_course_day_wise as $course_assing_day_info): ?>
-			<?php
-			//$i=1;
-			
-			
-			//$sql_day=mysql_query("select * from fbs_assign_course where semester_id='$txt_semister_id' and revised='//$batch_code'  group by course_date");
-			//while($row_day=mysql_fetch_assoc($sql_day)){
-				?>
-			 <tr>
-				<td valign="middle"><b><?php echo $day[$course_assing_day_info->day]; ?></b></td>
-				<td colspan="7">
-			 
-		   <table class="table table-striped table-bordered table-hover" width="100%">
-			 
-			
-			<?php
-			//$sql=mysql_query("select * from fbs_assign_course where semester_id='$txt_semister_id' and course_date='".$row_day['course_date']."'  and revised='$batch_code'");
-			//while($row=mysql_fetch_assoc($sql)){
-				
-			//$SQLCEHCKCOURSE=mysql_query("select * from  fbs_tabulation_sheet WHERE course_code='".$row['course_id']."' AND //student_id='$student_id'");
-			//$check_row_when_while_loop=mysql_fetch_assoc($SQLCEHCKCOURSE);
-			//echo $check_row_when_while_loop['course_code'];
-			//if($check_row_when_while_loop['course_code']!=$row['course_id']){
-			
-			?>
-				  
-			
-					
-					<tr class="odd gradeA">
-							<td width="5%">
-							 <input type="button" class="btn btn-success" value="Add" onclick="fnc_course_registration('<?php //echo //$row['course_id'].'##'.$txt_semister_id.'##'.$student_id.'##'.$row['section'].'##'.$batch.'##'.$row['course_date']; ?>')">
-							</td>
-
-							<td width="5%"><?php //echo $i; ?></td>
-							<td align="center" width="10%"><?php //echo $course_name_arrays[$row['course_id']]; ?> </td>
-							<td width="40%"><?php //echo $course_name_array[$row['course_id']]; ?> </td>
-							<td align="left" width="20%"><?php //echo $course_type_Arr[$course_name_arrays_type[$row['course_id']]]; ?> </td>
-							
-							<td align="center" width="10%"><?php //echo $Section_Arr[$row['section']]; ?> </td>
-							<td align="center" width="10%"><?php //echo $course_name_arrays_credit[$row['course_id']]; ?> </td>
-					</tr>
-			  
-					<?php
-					 // $i++;}
-					 // }
-				   
-					?>
-					 </table>
-				</td>
-				</tr>
-			<?php endforeach; ?>
-			
-		 </tbody>
-	</table>
-<script> 
-=======
 <script>
->>>>>>> c72e90a6ca1fdded7932fa44c3d3d1285ca7870a
 	
 	 function fnc_search_course()
 	 {
